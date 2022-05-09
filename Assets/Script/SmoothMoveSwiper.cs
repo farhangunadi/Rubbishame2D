89aaa5e8ece1f01c4e7905a1b0fccba705559faa
farhangunadi <farhan19004@mail.unpad.ac.id>
2022-05-09 22:53:39 +0700

    add menu and dummy gameplay

---
+++ Assets/Script/SmoothMoveSwiper.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothMoveSwiper : MonoBehaviour
+{
+    private Vector2 startTouchPosition, endTouchPosition;
+    private Vector3 startBinPosition, endBinPosition;
+    private float flyTime;
+    private float flightDuration = 0.1f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            startTouchPosition = Input.GetTouch(0).position;
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            endTouchPosition = Input.GetTouch(0).position;
+
+            if ((endTouchPosition.x < startTouchPosition.x) && transform.position.x > -1.75f)
+                StartCoroutine(Stand("left"));
+
+            if ((endTouchPosition.x > startTouchPosition.x) && transform.position.x < -1.75f)
+                StartCoroutine(Stand("right"));
+        }
+    }
+
+    private IEnumerator Stand(string whereToStand)
+    {
+        switch (whereToStand)
+        {
+            case "left":
+                flyTime = 0f;
+                startBinPosition = transform.position;
+                endBinPosition = new Vector3
+                    (startBinPosition.x - 1.75f, transform.position.y, transform.position.z);
+
+                while (flyTime < flightDuration)
+                {
+                    flyTime += Time.deltaTime;
+                    transform.position = Vector2.Lerp
+                        (startBinPosition, endBinPosition, flyTime / flightDuration);
+                    yield return null;
+                }
+                break;
+
+            case "right":
+                flyTime = 0f;
+                startBinPosition = transform.position;
+                endBinPosition = new Vector3
+                    (startBinPosition.x + 1.75f, transform.position.y, transform.position.z);
+
+                while (flyTime < flightDuration)
+                {
+                    flyTime += Time.deltaTime;
+                    transform.position = Vector2.Lerp
+                        (startBinPosition, endBinPosition, flyTime / flightDuration);
+                    yield return null;
+                }
+                break;
+        }
+    }
+}
---
+++ Assets/Script/SpawnTrash.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrash : MonoBehaviour
+{
+    [SerializeField] GameObject[] trashPrefab;
+    [SerializeField] float secondSpawn = 0.5f;
+    [SerializeField] float minTras;
+    [SerializeField] float maxTras;
+
+    void Start()
+    {
+        StartCoroutine(TrashSpawn());
+    }
+
+    IEnumerator TrashSpawn()
+    {
+        while (true)
+        {
+            var wanted = Random.Range(minTras, maxTras);
+            var position = new Vector3(wanted, transform.position.y);
+            GameObject gameObject = Instantiate(trashPrefab[Random.Range(0, trashPrefab.Length)],
+            position, Quaternion.identity);
+            yield return new WaitForSeconds(secondSpawn);
+            Destroy(gameObject, 5f);
+        }
+    }
+}
